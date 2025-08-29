b3920369b7b11ba6ced9ec44923658617ede9c94
Dhruv <17.dhruv.06@gmail.com>
2025-08-29 11:14:24 +0530

    Updated API with /test route and other changes

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Serve a simple HTML form for testing
-app.get("/test", (req, res) => {
+app.get("/BFHL", (req, res) => {
   res.send(`
     <!DOCTYPE html>
     <html>
